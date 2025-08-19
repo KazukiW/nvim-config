09469ef0166471a03b89cda2b4fb552dd383e9cd
Kazuki Wakata/CCS <wakatakz@ccsdm01.ccs-inc.co.jp>
2025-08-19 12:21:54 +0900

    Executed on 2025/08/19 Taking backup before replacing Win Neovim environment with Mac's one.

--- init.lua
+++ init.lua
@@ -42,7 +42,7 @@ require("lazy").setup({
     -- treesitter
     {
       "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
+      --build = ":TSUpdate",
       config = function()
         require("nvim-treesitter.configs").setup {
           ensure_installed = {
